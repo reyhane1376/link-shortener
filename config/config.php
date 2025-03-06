b922727f97a908991638a8234a4732cd3bcbbff7
reyhane <97376622+reyhane1376@users.noreply.github.com>
2025-03-06 15:24:53 +0330

    add config file

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+//config
+define('BASE_PATH', dirname(__DIR__));
+define('DISPLAY_ERROR', true);
+define('CURRENT_DOMAIN', trim(currentDomain(), '/'));
+define('DB_HOST', '127.0.0.1');
+define('DB_USERNAME', 'root');
+define('DB_NAME', 'link-shortener');
+define('DB_PASSWORD', '123456789');
